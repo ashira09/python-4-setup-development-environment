d6af7faa3985d1ce1ed3e98992a20db0abf355c4
ashira09 <vecnivtil89@gmail.com>
2026-04-17 22:17:01 +0800

    Сборка приложения в main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from init_db import categoryCRUD, bookCRUD, SessionLocal
+from sqlalchemy import text
+from fastapi import FastAPI, HTTPException
+from api.books import books_router
+from api.categories import categories_router
 
 print('Hello, World!')
 
@@ -13,3 +17,18 @@ with SessionLocal() as session:
     print('Добавленные категории: ', ', '.join(['"' + category.title + '"' for category in categories]))
     print('Книги категории "Фантастика": ', ', '.join(['"' + fiction_book.title + '"' for fiction_book in fiction_books]))
     print('Книги категории "Фэнтези": ', ', '.join(['"' + fantasy_book.title + '"' for fantasy_book in fantasy_books]))
+
+app = FastAPI()
+
+@app.get("/health")
+async def check_health():
+    try:
+        with SessionLocal() as session:
+            session.execute(text("SELECT 1"))
+    except Exception:
+        raise HTTPException(status_code=500, detail='Сервер не работоспособен')
+    finally:
+        raise HTTPException(status_code=200, detail='Сервер работоспособен')
+
+app.include_router(books_router, prefix="/api")
+app.include_router(categories_router, prefix="/api")
